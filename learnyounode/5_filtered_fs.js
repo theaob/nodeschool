030ffe3d4e51f8864c4ed01e2b6c0e2a1216dc0f
Onur Baykal <theaob@gmail.com>
2013-12-22 12:59:50 +0200

    fixed 5th challange

--- learnyounode/5_filtered_fs.js
+++ learnyounode/5_filtered_fs.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
+var fileExtension = process.argv[3];
 fs.readdir(process.argv[2], function(err, list)
 {
-	var fileExtension = process.argv[3];
-	for(var i; i < list.length; i++)
+	for(var i = 0; i < list.length; i++)
 	{
-		console.log(list[i].split('.'));
-		if(list[i].split('.')[1] == fileExtension)
+		var splittedList = list[i].split('.');
+		if(splittedList[1] === fileExtension)
 		{
 			console.log(list[i]);
 		}
